9577e2653ad6f303cdf987a5ef6ce5d5ca7cc8b1
Varadharajan <varathu09@gmail.com>
2020-08-30 19:36:25 +0530

    DEV- add token for jwt auth

---
+++ Domain/Models/UserInfo.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Supermarket.API.Domain.Models
+{
+    public partial class UserInfo
+    {
+       
+            public int UserId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public DateTime CreatedDate { get; set; }
+        
+    }
+}
